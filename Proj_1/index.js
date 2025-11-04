f8103d2e4444e11a9fcb0d67cd2949a0ec6af779
Jakub Zima <jakubzima@student.agh.edu.pl>
2025-11-04 19:33:01 +0100

    Projekt do Lab1

--- Proj_1/index.js
+++ Proj_1/index.js
@@ -1,3 +1,26 @@
-setTimeout(() => {
-    console.log('Test JS')
-}, 4000)
+console.log("Witamy na stronie FC Barcelony!");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("contactForm");
+  const message = document.getElementById("formMessage");
+
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const email = document.getElementById("email").value.trim();
+      const subject = document.getElementById("subject").value;
+      const text = document.getElementById("message").value.trim();
+
+      if (!email || !subject || !text) {
+        message.style.color = "red";
+        message.textContent = "❌ Proszę wypełnić wszystkie pola.";
+        return;
+      }
+
+      message.style.color = "#edbb00";
+      message.textContent = "✅ Wiadomość została wysłana pomyślnie!";
+      form.reset();
+    });
+  }
+});
